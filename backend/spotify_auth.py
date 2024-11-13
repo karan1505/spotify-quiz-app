3bf0a3b0a017a9fc9c58d19d4738a9efec1ff8c9
srinath-ganesh <srinath22.g@gmail.com>
2024-11-12 23:21:32 -0600

    URL Changes Once More

--- backend/spotify_auth.py
+++ backend/spotify_auth.py
@@ -5,7 +5,7 @@ def create_spotify_client():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
         client_id="e24a1c8aefb543f2845f9b60266f7a7e",
         client_secret="985edfaea59c43698006e0c6059b94a3",
-        redirect_uri="http://localhost:8000/redirect",
+        redirect_uri="https://spotify-quiz-app-abuw.onrender.com/redirect",
         scope="user-read-playback-state user-read-currently-playing"
     ))
     return sp
