6a5245b85a2ca85796215281019e4dc048f49de8
Karan Sreedhar <79150373+karan1505@users.noreply.github.com>
2024-12-12 22:01:04 -0600

    WelcomePageUpdate

--- frontend/src/webpages/WelcomePage.js
+++ frontend/src/webpages/WelcomePage.js
@@ -76,7 +76,7 @@ const WelcomePage = () => {
               boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.3)",
             }}
           >
-            Troubleshooting
+            Instructions and Troubleshooting
           </Button>
           <Button
             variant="contained"
